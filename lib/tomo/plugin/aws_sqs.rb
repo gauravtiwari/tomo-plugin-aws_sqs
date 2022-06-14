7d9304c786cb8f69ca7649cf8de451c6d08be2bc
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-06-14 17:14:30 +0100

    Rubocop 🚓

--- lib/tomo/plugin/aws_sqs.rb
+++ lib/tomo/plugin/aws_sqs.rb
@@ -9,5 +9,5 @@ module Tomo::Plugin::AwsSqs
   defaults aws_sqs_systemd_service: "aws_sqs_%{application}.service",
            aws_sqs_systemd_service_path: ".config/systemd/user/%{aws_sqs_systemd_service}",
            aws_sqs_systemd_service_template_path: File.expand_path("aws_sqs/service.erb", __dir__),
-           aws_sqs_systemd_command: 'bundle exec aws_sqs_active_job --queue default'
+           aws_sqs_systemd_command: "bundle exec aws_sqs_active_job --queue default"
 end
